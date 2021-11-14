054902c4abef0952f6a582f4a2fcda30965745d8
venance-r <83973972+venance-r@users.noreply.github.com>
2021-11-14 13:54:19 -0800

    Update display

--- app.R
+++ app.R
@@ -1,41 +1,56 @@
 library(shiny)
+library(shinydashboard)
 
 
 source("./data_prep_BQ.R")
 
 
 # Define UI ----
-ui <- fluidPage( 
-  titlePanel("SVAR"),
+ui <- dashboardPage(
+  dashboardHeader(title = "Structural VAR"),
   
-  sidebarPanel(
-    selectInput("select_impulse", h3("Impulse shock"), 
-                choices = list("Supply" = 1, "Demand" = 2), selected = 1),
+  dashboardSidebar( disable = T),
   
-    selectInput("select_response", h3("Response variable"), 
-              choices = list("Output" = 1, "Unemployment" = 2), selected = 1),
+  dashboardBody(
     
-    sliderInput("select_horizon", h3("Time horizon"),
-                min = 10, max = 60, value = 40, step = 2, ticks=F),
-    
-    radioButtons("sign_shock", h3("Sign of the shock"),
-                 choices = list("Positive" = 1, "Negative" = -1), selected = 1),
-    
-    h3("Bootstrap"),
-    checkboxInput("bootstrap", "Compute CI", value = F), 
-  
-    conditionalPanel(
-      condition = "input.bootstrap == true",
-      sliderInput("no_boot", h5("Confidence interval band "),
-                  min = 0.9, max = 0.99, value = 0.95, ticks=F, step= 0.01)
-    )),
-    
-    mainPanel(p("SVAR model with long run restrictions. Replication of Blanchard, O. J., & Quah, D. (1989). The dynamic effects of aggregate demand and supply disturbances. American Economic Review, 79(4), 655-673.")),
-    mainPanel(h4(textOutput("title"))),
-    mainPanel(plotOutput("plot")),
-    mainPanel(p(textOutput("ci_description")))
+    fluidRow(
+      
+      box(width = 3,
+          selectInput("select_impulse", h3("Impulse shock"), 
+                      choices = list("Supply" = 1, "Demand" = 2), selected = 1),
+          
+          selectInput("select_response", h3("Response variable"), 
+                      choices = list("Output" = 1, "Unemployment" = 2), selected = 1),
+          
+          sliderInput("select_horizon", h3("Time horizon (Quarters)"),
+                      min = 10, max = 60, value = 40, step = 2, ticks=F),
+          
+          radioButtons("sign_shock", h3("Sign of the shock"),
+                       choices = list("Positive" = 1, "Negative" = -1), selected = 1),
+          
+          h3("Bootstrap"),
+          checkboxInput("bootstrap", "Compute CI", value = F), 
+          
+          conditionalPanel(
+            condition = "input.bootstrap == true",
+            sliderInput("no_boot", h5("Confidence interval band "),
+                        min = 0.9, max = 0.99, value = 0.95, ticks=F, step= 0.01)
+          )
+      ),
+      
+      
+      box(width = 9,
+          h4(textOutput("title")),
+          plotOutput("plot"),
+          p(textOutput("ci_description")),
+          "Structural VAR model with long run restrictions. Replication of Blanchard, O. J., & Quah, D. (1989). The dynamic effects of aggregate demand and supply disturbances. American Economic Review, 79(4), 655-673.",
+          br(), "Data source: ", tags$a(href="https://fred.stlouisfed.org/", "FRED")
+      )
+      
+    )
   )
-  
+)
+
 
 
 # Define server logic ----
@@ -51,7 +66,7 @@ server <- function(input, output) {
     }
   })
   
-
+  
   output$plot <- renderPlot({
     
     # Unpack inputs
@@ -64,18 +79,18 @@ server <- function(input, output) {
     
     # IRF of GDP response must be cumulative to recover level
     cumu <- switch(rep, "gdp" = T, "unrate" = F ) 
-
+    
     # Compute IRF
     
     irf <- irf(svar_results, impulse = imp, response = rep, n.ahead = hor,
-                boot = bootstrap, ci = no_boot, cumulative = cumu)
+               boot = bootstrap, ci = no_boot, cumulative = cumu)
     sirf <- sign_shock*data.frame(irf$irf)
     
     # Plot
     plot_irf <- ggplot(sirf) +
       geom_line(aes(x = c(0:hor), y = sirf[,1]), size = 0.9) +
       labs(x = "Quarters", y = switch(rep, "gdp" = "Output", "unrate" = "Unemployment"))
-                                            
+    
     
     # Option Bootstrap 
     if (bootstrap==T) {
